a4cf9a3961d0508a4b7178d83e2c686abe17affb
Tronaluz <lucas_tron@hotmail.com>
2023-01-15 14:13:06 -0300

    update final

--- script.js
+++ script.js
@@ -18,14 +18,25 @@ slider.oninput = function(){
 }
 
 
-function generatePass(){
+function generatePassword(){
    let pass = "";
-
-   for(let i = 0; n = charset.length; i < sliderElement.value; ++i){
-     pass +=  charset.length.charAt(Math.floor(Math.random() *n)); 
+   for(let i = 0, n =charset.length; i < sliderElement.value; ++i){
+    pass += charset.charAt(Math.floor(Math.random() * n))
+    
    }
 
-   containerPassword.classList.remove("hide"); 
-   password.innerHTML = pass;
-
+   containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha = pass;
+    
 }
+
+function copyPassword(){
+  navigator.clipboard.writeText(novaSenha)
+    .then(() => {
+      alert("A senha foi copiada 😁");
+    })
+    .catch(err => {
+      console.error('Failed to copy: ', err);
+    });
+  }
