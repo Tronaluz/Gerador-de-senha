37ffeb5bfe2c4980ec57c33b2313399f17d9b3db
Tronaluz <lucas_tron@hotmail.com>
2023-01-15 00:16:12 -0300

    Update

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("TESTE")
+
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button")
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*";
+let novaSenha = "";
+
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function(){
+    sizePassword.innerHTML = this.value;
+}
+
+
+function generatePass(){
+   let pass = "";
+
+   for(let i = 0; n = charset.length; i < sliderElement.value; ++i){
+     pass +=  charset.length.charAt(Math.floor(Math)) 
+   }
+}
